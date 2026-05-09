e7bf3e702e91d0419d99f3bf7ebed8a4cbd5bfa7
wj <wongjuanaa@gmail.com>
2026-05-09 17:53:59 -0400

    simplify/refactor tailwind warnings

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -7,7 +7,7 @@ import { Calendar } from "@/components/civiq/Calendar";
 
 export default function MapPage() {
     return (
-        <div className="relative flex min-h-full flex-1 flex-col overflow-hidden bg-slate-50 dark:bg-[var(--background)]">
+        <div className="relative flex min-h-full flex-1 flex-col overflow-hidden bg-slate-50 dark:bg-background">
             <Header />
             <main className="relative z-10 flex-1 py-12 px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-7xl">
